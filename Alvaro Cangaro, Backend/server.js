9e756e54e0e6e05e9efd93d92ceaed6024af702d
AlvaroCangaro <95660827+AlvaroCangaro@users.noreply.github.com>
2022-06-16 23:46:12 -0300

    Tp 4

---
+++ Alvaro Cangaro, Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const rutas = require('./routes/productos');
+const app = express();
+const path = require('path');
+
+app.set("json spaces", 2)
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use('/html', express.static('html'))
+
+app.use("/api/productos", rutas)
+
+const server = app.listen(8080, () => {
+    console.log(`Servidor prendido en el puerto ${server.address().port}`)
+})
+server.on('error', err => console.log(`Error en el server ${err}`))
