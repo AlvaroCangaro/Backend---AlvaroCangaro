538a23e40149c91925aaa1ba62287c9ef63db07e
AlvaroCangaro <95660827+AlvaroCangaro@users.noreply.github.com>
2022-06-24 21:57:06 -0300

    Fixing

--- Alvaro Cangaro, Backend/src/app.js
+++ Alvaro Cangaro, Backend/src/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
-const productosRoute = require("./routes/productos");
-const indexRoute = require("./routes/index");
+const indexApiRest = require("./routes/indexApiRest");
+const indexApi = require("./routes/indexApi");
 const path = require("path");
 const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+// handlebars settings
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+        defaultLayout: path.join(__dirname, "./views/layout/main.hbs"),
+        layoutsDir: path.join(__dirname, "./views/layout"),
+        partialsDir: path.join(__dirname, "./views/partials"),
+    })
+);
+
 
 // settings
 app.set("port", 8080);
 app.set("json spaces", 2);
-// app.set("views", path.join(__dirname, "views"));// ejs
+app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");// ejs
-app.set("views", path.join(__dirname, "./views")); // pug
-app.set("view engine", "pug");// pug
+// app.set("view engine", "pug");// pug
+app.set("view engine", "hbs"); // hbs
+
 
 // middleware
 app.use(morgan("dev"));
@@ -19,12 +33,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // routes
-app.use("/", indexRoute);
-app.use("/api/productos", productosRoute);
+app.use("/", indexApi);
+app.use("/api/productos", indexApiRest);
 
-// statics
-app.use(express.static(path.join(__dirname, "public")));
-// app.use('/html', express.static('html'))
+// staticsnp
+app.use(express.static(path.join(__dirname, "./public")));
 
 // 404 handler
 app.use((req, res, next) => {
--- Alvaro Cangaro, Backend/src/server.js
+++ Alvaro Cangaro, Backend/src/server.js
@@ -1,7 +1,5 @@
 const app = require('./app')
 
-// starting server
-
 const main = async () => {
     await app.listen(app.get('port'))
     console.log(
@@ -10,26 +8,3 @@ const main = async () => {
 }
 
 main()
-
-
-
-
-// const express = require('express');
-// const rutas = require('./routes/productos');
-// const app = express();
-// const path = require('path');
-
-// app.set("json spaces", 2)
-// app.set('views', path.join(__dirname, './views'))
-// app.set('view engine', 'pug')
-
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
-// app.use('/html', express.static('html'))
-
-// app.use("/api/productos", rutas)
-
-// const server = app.listen(8080, () => {
-//     console.log(`Servidor HTTP conectado, escuchando en el puerto ${server.address().port}`)
-// })
-// server.on('error', err => console.log(`Error en servidor: ${err}`))
