bc002692393b510b673a6766ca76244a640a819b
AlvaroCangaro <95660827+AlvaroCangaro@users.noreply.github.com>
2022-06-30 20:14:42 -0300

    Fixing

--- Alvaro Cangaro, Backend/src/routes/indexApi.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { productsView, formView, formSubmit } = require("../controllers/apiControllers");
-const router = Router(); //Router
-
-router.get("/", formView);
-router.get("/productos", productsView)
-router.post("/productos", formSubmit);
-
-module.exports = router;
